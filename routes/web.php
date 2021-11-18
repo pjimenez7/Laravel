ed2f3ecd956f25895befd2302c46e9a841a082d6
Pedro Jimenez Martin <pjimenez@altracorporacion.es>
2021-11-18 08:00:21 +0100

    Mi primer commit para Airzone

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/insertar", function(){
+    DB::insert('insert into articulos (nombre_articulo, precio, pais_origen, seccion, observaciones) values (?,?,?,?,?)',
+     ["Jarron", 15.4, "Japon", "Ceramica", "Ganga"]);
+    
+});
